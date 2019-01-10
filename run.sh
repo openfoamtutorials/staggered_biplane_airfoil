6a83579aba081b8fb12ece93b9ddc12410dba212
Robert Lee <rlee32@gatech.edu>
2019-01-10 16:26:02 -0500

    added necessary flags to gmsh command for openfoam compatiblity with gmsh 4.0+; added sample commands for parallel run.

--- run.sh
+++ run.sh
@@ -4,11 +4,15 @@
 set -e
 
 # Make the mesh:
-gmsh -3 -o main.msh mesh/main.geo
+gmsh -format msh2 -3 -o main.msh mesh/main.geo
 # Convert the mesh to OpenFOAM format:
 gmshToFoam main.msh -case case
 # Adjust polyMesh/boundary:
 changeDictionary -case case
 # Finally, run the simulation:
 simpleFoam -case case
+
+# Parallel
+#decomposePar -case case
+#mpirun -np 2 simpleFoam -case case -parallel
 
